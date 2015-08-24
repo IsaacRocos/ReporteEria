2daa0fa385990e098bf6ea2c1b260cf5548e4227
Isaac Rodríguez <angelrc@live.com.mx>
2015-08-24 12:53:47 -0500

    Implementada secuencia para generar todos los rportes de estado

--- src/reporteencuestadores/Principal.java
+++ src/reporteencuestadores/Principal.java
@@ -5,14 +5,53 @@
  */
 package reporteencuestadores;
 
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  *
  * @author AGENTE005
  */
 public class Principal {
+
     public static void main(String[] args) {
-        ReporteEncuestadores re = new ReporteEncuestadores();
-        re.ejecutarConsulta("SELECT "
+        String entidades[] = {"ZACATECAS",
+            "YUCATÁN",
+            "VERACRUZ DE IGNACIO DE LA LLAVE",
+            "TLAXCALA",
+            "TAMAULIPAS",
+            "TABASCO",
+            "SONORA",
+            "SINALOA",
+            "SAN LUIS POTOSÍ",
+            "QUINTANA ROO",
+            "QUERÉTARO",
+            "PUEBLA",
+            "OAXACA",
+            "NUEVO LEÓN",
+            "NAYARIT",
+            "MÉXICO",
+            "MORELOS",
+            "MICHOACÁN DE OCAMPO",
+            "JALISCO",
+            "HIDALGO",
+            "GUERRERO",
+            "GUANAJUATO",
+            "DURANGO",
+            "DISTRITO FEDERAL",
+            "COLIMA",
+            "COAHUILA DE ZARAGOZA",
+            "CHIHUAHUA",
+            "CHIAPAS",
+            "CAMPECHE",
+            "BAJA CALIFORNIA SUR",
+            "BAJA CALIFORNIA",
+            "AGUASCALIENTES"};
+
+        for (String estado : entidades) {
+            ReporteEncuestadores re = new ReporteEncuestadores();
+            System.out.println("Generando reporte para " + estado);
+            re.ejecutarConsulta("SELECT "
                     + " A.LONGITUD ,"
                     + "	A.LATITUD,"
                     + "	A.ENCUESTA_ID, "
@@ -20,6 +59,8 @@ public class Principal {
                     + " A.NOM_ENT, "
                     + " A.NOM_MUN, "
                     + " A.NOM_LOC, "
-                    + " A.CVE_LOC FROM registros_eria.registros as A where A.NOM_ENT = \"CHIAPAS\" ORDER BY NOM_ENC, ENCUESTA_ID");
+                    + " A.CVE_LOC FROM registros_eria.registros as A where A.NOM_ENT = \"" + estado + " \" ORDER BY NOM_ENC, ENCUESTA_ID");
+            System.out.println(estado + ": Reporte creado.\n");
+        }
     }
 }
